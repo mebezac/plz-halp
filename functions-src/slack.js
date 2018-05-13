880560f5a2f7fc87017decc2fd46b932f774f9ea
Zac Clay <zacclay@gmail.com>
2018-05-12 22:22:45 -0400

    Add json header

--- functions-src/slack.js
+++ functions-src/slack.js
@@ -1,6 +1,7 @@
 exports.handler = function(event, context, callback) {
   callback(null, {
     statusCode: 200,
+    "headers": { 'Content-Type': 'application/json' },
     body: JSON.stringify({
     "response_type": "in_channel",
     "text": "It's 80 degrees right now.",
